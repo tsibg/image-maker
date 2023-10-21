0ab6297f498cb04b157f4eed12a4c1d4e15845b3
tsibg <tsi.web.design@gmail.com>
2023-10-21 13:56:31 +0300

    isLoading state to the image component

--- src/app/(generator)/generated-image.js
+++ src/app/(generator)/generated-image.js
@@ -2,20 +2,26 @@ import React, { Suspense } from "react";
 import Image from "next/image";
 import styles from "./generator.module.css";
 
-export default function GeneratedImage({ prompt, imageUrl }) {
+export default function GeneratedImage({ isLoading, prompt, imageUrl }) {
   React.useEffect(() => {
     import("@dotlottie/player-component");
   });
   return (
-    <div className={`${styles.imageWrapper} ${imageUrl? styles.ready :  styles.loading} `}>
-        {/* {!imageUrl && (<dotlottie-player
+    <div
+      className={`${styles.imageWrapper} 
+      ${imageUrl && styles.ready} 
+      ${isLoading && styles.loading}`}
+    >
+      {!isLoading && (
+        <dotlottie-player
           src="./lottie/loading.lottie"
           autoplay
           loop
           style={{ height: "100%", width: "100%" }}
-        ></dotlottie-player>)} */}
-        
-        {imageUrl &&<Image src={imageUrl} quality={100} fill alt={prompt} />}
+        ></dotlottie-player>
+      )}
+
+      {imageUrl && <Image src={imageUrl} quality={100} fill alt={prompt} />}
     </div>
   );
 }
